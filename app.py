f3c433e71e8c210693ca29c8caf478f0f0982708
tantinoroj <tan.t@chevron.com>
2025-05-02 14:33:26 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,58 @@
-from os import environ
+import identity.web
+import requests
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_session import Session
 
+import app_config
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+app = Flask(__name__)
+app.config.from_object(app_config)
+Session(app)
+
+auth = identity.web.Auth(
+    session=session,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+)
+
+@app.route("/login")
+def login():
+    return render_template("login.html", **auth.log_in(
+        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
+        redirect_uri=url_for("auth_response", _external=True)
+    ))
+
+@app.route(app_config.REDIRECT_PATH)
+def auth_response():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return render_template("auth_error.html", result=result)
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+def logout():
+    return redirect(auth.log_out(url_for("index", _external=True)))
+
+@app.route("/")
+def index():
+    user = auth.get_user()
+    if not user:
+        return redirect(url_for("login"))
+    return render_template("index.html", user=user)
+
+@app.route("/call_downstream_api")
+def call_downstream_api():
+    token = auth.get_token_for_user(app_config.SCOPE)
+    if "error" in token:
+        return redirect(url_for("login"))
+    # Use access token to call downstream api
+    api_result = requests.get(
+        app_config.ENDPOINT,
+        headers={'Authorization': 'Bearer ' + token['access_token']},
+        timeout=30,
+    ).json()
+    return render_template('display.html', result=api_result)
+
+if __name__ == "__main__":
+    app.run()
